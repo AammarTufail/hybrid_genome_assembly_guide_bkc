3e986f0aa52cd0365d775e39b1a47927da4cb9b2
AammarTufail <m.aammar.tufail@outlook.com>
2025-12-26 23:54:18 +0500

    readme and installation and other scripts updated.

--- analysis.sh
+++ analysis.sh
@@ -1,1 +1,272 @@
+#!/bin/bash
 
+# initialize conda for this script
+eval "$(conda shell.bash hook)"
+
+# rename raw reads
+mv 01_raw_reads/short_reads/*_1.fastq.gz 01_raw_reads/short_reads/codanics_1.fastq.gz
+mv 01_raw_reads/short_reads/*_2.fastq.gz 01_raw_reads/short_reads/codanics_2.fastq.gz
+mv 01_raw_reads/long_reads/*.fastq.gz 01_raw_reads/long_reads/codanics_long.fastq.gz
+
+################## directories setup #####################
+# QC before processing
+mkdir -p 02_reads_QC_before_processing
+mkdir -p 02_reads_QC_before_processing/short_reads
+mkdir -p 02_reads_QC_before_processing/long_reads
+
+# processed reads
+mkdir -p 03_reads_processed
+mkdir -p 03_reads_processed/short_reads
+mkdir -p 03_reads_processed/long_reads
+
+# QC after processing
+mkdir -p 04_reads_QC_after_processing
+mkdir -p 04_reads_QC_after_processing/short_reads
+mkdir -p 04_reads_QC_after_processing/long_reads
+
+# hybrid genome assembly
+mkdir -p 05_hybrid_genome_assembly
+
+# Genome quality assessment
+mkdir -p 06_genome_quality_assessment
+mkdir -p 06_genome_quality_assessment/01_checkm2
+mkdir -p 06_genome_quality_assessment/01_checkm2/01_short_only_assembly
+mkdir -p 06_genome_quality_assessment/01_checkm2/02_long_only_assembly
+mkdir -p 06_genome_quality_assessment/01_checkm2/03_hybrid_assembly
+mkdir -p 06_genome_quality_assessment/02_quast
+mkdir -p 06_genome_quality_assessment/02_quast/01_short_only_assembly
+mkdir -p 06_genome_quality_assessment/02_quast/02_long_only_assembly
+mkdir -p 06_genome_quality_assessment/02_quast/03_hybrid_assembly
+mkdir -p 06_genome_quality_assessment/03_busco
+mkdir -p 06_genome_quality_assessment/03_busco/01_short_only_assembly
+mkdir -p 06_genome_quality_assessment/03_busco/02_long_only_assembly
+mkdir -p 06_genome_quality_assessment/03_busco/03_hybrid_assembly
+
+# Genome annotation
+mkdir -p 07_genome_annotation
+mkdir -p 07_genome_annotation/01_prokka
+mkdir -p 07_genome_annotation/01_prokka/01_short_only_assembly
+mkdir -p 07_genome_annotation/01_prokka/02_long_only_assembly
+mkdir -p 07_genome_annotation/01_prokka/03_hybrid_assembly
+mkdir -p 07_genome_annotation/02_bakta
+mkdir -p 07_genome_annotation/02_bakta/01_short_only_assembly
+mkdir -p 07_genome_annotation/02_bakta/02_long_only_assembly
+mkdir -p 07_genome_annotation/02_bakta/03_hybrid_assembly
+
+
+############## Short reads quality checking tools #####################
+# run fastqc
+conda activate 01_short_read_qc
+fastqc -o 02_reads_QC_before_processing/short_reads \
+    --extract --svg -t 12 \
+    01_raw_reads/short_reads/*.fastq.gz
+
+# create multiqc output directory
+mkdir -p 02_reads_QC_before_processing/short_reads/multiqc/
+conda activate 02_multiqc
+#expert use case of multiqc
+multiqc -p \
+    -o 02_reads_QC_before_processing/short_reads/multiqc/ \
+    02_reads_QC_before_processing/short_reads/
+
+############## fastp #####################
+conda activate 01_short_read_qc
+fastp \
+    -i 01_raw_reads/short_reads/codanics_1.fastq.gz -I 01_raw_reads/short_reads/codanics_2.fastq.gz \
+    -o 03_reads_processed/short_reads/codanics_1_processed.fastq.gz -O 03_reads_processed/short_reads/codanics_2_processed.fastq.gz \
+    -q 25 \
+    -h 04_reads_QC_after_processing/short_reads/fastp_report.html \
+    -j 04_reads_QC_after_processing/short_reads/fastp_report.json \
+    -w 12
+
+
+################## long reads qc #####################
+conda activate 03a_long_read_nanoplot
+NanoPlot \
+    --fastq 01_raw_reads/long_reads/codanics_long.fastq.gz \
+    -o 02_reads_QC_before_processing/long_reads/ \
+    --threads 12
+
+################## long reads processing #####################
+# Nanofilt
+conda activate 03b_long_read_nanofilt
+zcat 01_raw_reads/long_reads/codanics_long.fastq.gz | \
+    NanoFilt -q 8 --length 1000 --headcrop 50 | gzip > \
+    03_reads_processed/long_reads/codanics_long_filtered.fastq.gz
+
+# Filtlong
+# conda activate 03c_long_read_filtlong
+# Filtlong \
+#     --min_length 1000 \
+#     --keep_percent 90 \
+#     --target_bases 1000000000 \
+#     03_reads_processed/long_reads/codanics_long_filtered.fastq.gz | \
+#     gzip > 03_reads_processed/long_reads/codanics_long_processed.fastq.gz
+
+################## long reads qc after processing #####################
+conda activate 03a_long_read_nanoplot
+NanoPlot \
+    --fastq 03_reads_processed/long_reads/codanics_long_filtered.fastq.gz \
+    -o 04_reads_QC_after_processing/long_reads/ \
+    --threads 12
+
+################## hybrid genome assembly #####################
+conda activate 04_unicycler
+
+mkdir -p 05_hybrid_genome_assembly/01_short_only_assembly
+# short reads only assembly
+unicycler \
+    -1 03_reads_processed/short_reads/codanics_1_processed.fastq.gz \
+    -2 03_reads_processed/short_reads/codanics_2_processed.fastq.gz \
+    -o 05_hybrid_genome_assembly/01_short_only_assembly/ \
+    -t 10
+# long read only
+mkdir -p 05_hybrid_genome_assembly/02_long_only_assembly
+unicycler \
+    -l 03_reads_processed/long_reads/codanics_long_filtered.fastq.gz \
+    -o 05_hybrid_genome_assembly/02_long_only_assembly/ \
+    -t 10
+# hybrid assembly
+mkdir -p 05_hybrid_genome_assembly/03_hybrid_assembly
+unicycler \
+    -1 03_reads_processed/short_reads/codanics_1_processed.fastq.gz \
+    -2 03_reads_processed/short_reads/codanics_2_processed.fastq.gz \
+    -l 03_reads_processed/long_reads/codanics_long_filtered.fastq.gz \
+    -o 05_hybrid_genome_assembly/03_hybrid_assembly/ \
+    -t 10 \
+    --verbosity 2 \
+    --min_fasta_length 500
+
+
+################## genome quality assessment using checkm2 #####################
+conda activate 04a_checkm2
+export CHECKM2DB="/home/codanics/databases_important/checkm2_db/CheckM2_database/uniref100.KO.1.dmnd"
+# short reads only assembly
+checkm2 predict \
+    --threads 10 \
+    --input 05_hybrid_genome_assembly/01_short_only_assembly/assembly.fasta \
+    --output-directory 06_genome_quality_assessment/01_checkm2/01_short_only_assembly/
+# long reads only assembly
+checkm2 predict \
+    --threads 10 \
+    --input 05_hybrid_genome_assembly/02_long_only_assembly/assembly.fasta \
+    --output-directory 06_genome_quality_assessment/01_checkm2/02_long_only_assembly/
+# hybrid assembly
+checkm2 predict \
+    --threads 10 \
+    --input 05_hybrid_genome_assembly/03_hybrid_assembly/assembly.fasta \
+    --output-directory 06_genome_quality_assessment/01_checkm2/03_hybrid_assembly/
+
+
+
+################## genome quality assessment using quast #####################
+conda activate 04b_quast
+# short reads only assembly
+quast \
+    -o 06_genome_quality_assessment/02_quast/01_short_only_assembly/quast_results \
+    -t 10 \
+    05_hybrid_genome_assembly/01_short_only_assembly/assembly.fasta
+    --circos --glimmer --rna-finding \
+    --conserved-genes-finding \
+    --report-all-metrics \
+    --use-all-alignments
+# long reads only assembly
+quast \
+    -o 06_genome_quality_assessment/02_quast/02_long_only_assembly/quast_results \
+    -t 10 \
+    05_hybrid_genome_assembly/02_long_only_assembly/assembly.fasta
+    --circos --glimmer --rna-finding \
+    --conserved-genes-finding \
+    --report-all-metrics \
+    --use-all-alignments
+# hybrid assembly
+quast \
+    -o 06_genome_quality_assessment/02_quast/03_hybrid_assembly/quast_results \
+    -t 10 \
+    05_hybrid_genome_assembly/03_hybrid_assembly/assembly.fasta
+    --circos --glimmer --rna-finding \
+    --conserved-genes-finding \
+    --report-all-metrics \
+    --use-all-alignments
+
+################## genome quality assessment using busco #####################
+conda activate 04c_busco
+# short reads only assembly
+busco \
+    -i 05_hybrid_genome_assembly/01_short_only_assembly/assembly.fasta \
+    -o 06_genome_quality_assessment/03_busco/01_short_only_assembly/busco_results \
+    -l bacteria_odb12 \
+    -m genome \
+    -c 10
+busco --plot 06_genome_quality_assessment/03_busco/01_short_only_assembly/busco_results
+# long reads only assembly
+busco \
+    -i 05_hybrid_genome_assembly/02_long_only_assembly/assembly.fasta \
+    -o 06_genome_quality_assessment/03_busco/02_long_only_assembly/busco_results \
+    -l bacteria_odb12 \
+    -m genome \
+    -c 10
+busco --plot 06_genome_quality_assessment/03_busco/02_long_only_assembly/busco_results
+# hybrid assembly
+busco \
+    -i 05_hybrid_genome_assembly/03_hybrid_assembly/assembly.fasta \
+    -o 06_genome_quality_assessment/03_busco/03_hybrid_assembly/busco_results \
+    -l bacteria_odb12 \
+    -m genome \
+    -c 10
+busco --plot 06_genome_quality_assessment/03_busco/03_hybrid_assembly/busco_results
+
+################### genome annotation #####################
+# prokka annotation
+conda activate 05_genome_annotation
+# short reads only assembly
+prokka --outdir 07_genome_annotation/01_prokka/01_short_only_assembly/ \
+    --prefix codanics_prokka_short_only \
+    --kingdom Bacteria \
+    --addgenes --cpus 10 \
+    05_hybrid_genome_assembly/01_short_only_assembly/assembly.fasta \
+    --force
+
+# long reads only assembly
+prokka --outdir 07_genome_annotation/01_prokka/02_long_only_assembly/ \
+    --prefix codanics_prokka_long_only \
+    --kingdom Bacteria \
+    --addgenes --cpus 10 \
+    05_hybrid_genome_assembly/02_long_only_assembly/assembly.fasta \
+    --force
+
+# hybrid assembly
+prokka --outdir 07_genome_annotation/01_prokka/03_hybrid_assembly/ \
+    --prefix codanics_prokka_hybrid \
+    --kingdom Bacteria \
+    --addgenes --cpus 10 \
+    05_hybrid_genome_assembly/03_hybrid_assembly/assembly.fasta \
+    --force
+
+
+# bakta annotation (this will automatically create new directory for output)
+conda activate 05_genome_annotation
+# short reads only assembly
+bakta \
+    05_hybrid_genome_assembly/01_short_only_assembly/assembly.fasta \
+    --db /home/codanics/databases_important/bakta_db/db-light \
+    -t 10 --verbose \
+    -o 07_genome_annotation/02_bakta/ \
+    --prefix codanics_bakta_short_only \
+    --complete --force
+# long reads only assembly
+bakta \
+    05_hybrid_genome_assembly/02_long_only_assembly/assembly.fasta \
+    --db /home/codanics/databases_important/bakta_db/db-light \
+    -t 10 --verbose \
+    -o 07_genome_annotation/02_bakta/ \
+    --prefix codanics_bakta_long_only \
+    --complete --force
+# hybrid assembly
+bakta \
+    05_hybrid_genome_assembly/03_hybrid_assembly/assembly.fasta \
+    --db /home/codanics/databases_important/bakta_db/db-light \
+    -t 10 --verbose \
+    -o 07_genome_annotation/02_bakta/ \
+    --prefix codanics_bakta_hybrid \
+    --complete --force
--- readme.md
+++ readme.md
@@ -1,11 +1,65 @@
-# Whole Genome Sequence and Assemly
+# Whole Genome Sequencing and Assembly Guide
+
+This guide provides a comprehensive, step-by-step workflow for assembling bacterial genomes using short reads, long reads, and hybrid approaches. It covers raw data acquisition, quality control, assembly, quality assessment, and annotation, with all commands and scripts provided for reproducibility.
+
+---
+
+## Table of Contents
+
+- [Whole Genome Sequencing and Assembly Guide](#whole-genome-sequencing-and-assembly-guide)
+  - [Table of Contents](#table-of-contents)
+  - [Overview](#overview)
+  - [Workflow Summary](#workflow-summary)
+  - [Step 1: Download Raw Reads](#step-1-download-raw-reads)
+    - [1.1. Install grabseqs](#11-install-grabseqs)
+    - [1.2. Download raw reads](#12-download-raw-reads)
+  - [Step 2: Tool Installation](#step-2-tool-installation)
+    - [2.1. Run the installation script](#21-run-the-installation-script)
+  - [Step 3: Data Preparation and Quality Control](#step-3-data-preparation-and-quality-control)
+    - [3.1. Rename and organize raw reads](#31-rename-and-organize-raw-reads)
+    - [3.2. Short Read Quality Control](#32-short-read-quality-control)
+    - [3.3. Short Read Preprocessing](#33-short-read-preprocessing)
+    - [3.4. Long Read Quality Control](#34-long-read-quality-control)
+    - [3.5. Long Read Preprocessing](#35-long-read-preprocessing)
+  - [Step 4: Genome Assembly](#step-4-genome-assembly)
+  - [Step 5: Genome Quality Assessment](#step-5-genome-quality-assessment)
+  - [Step 6: Genome Annotation](#step-6-genome-annotation)
+  - [Running the Analysis](#running-the-analysis)
+  - [Script Reference](#script-reference)
+  - [Notes](#notes)
+  - [Troubleshooting](#troubleshooting)
+  - [Acknowledgements](#acknowledgements)
+
+---
 
 ## Overview
-## Workflow
-### 1. Raw Reads
 
- We will use grabseqs for raw reads downloading.
-#### 1.1. Install grabseqs
+Genome assembly is the process of reconstructing a genome from sequencing reads. This guide demonstrates three approaches:
+
+- **Short-read assembly** (Illumina)
+- **Long-read assembly** (Nanopore/PacBio)
+- **Hybrid assembly** (combining both)
+
+We use conda environments for reproducible tool installation and provide bash scripts for each analysis step.
+
+---
+
+## Workflow Summary
+
+1. **Download raw reads** (using grabseqs)
+2. **Install required tools** (via conda)
+3. **Quality control and preprocessing** (FastQC, fastp, NanoPlot, NanoFilt)
+4. **Genome assembly** (Unicycler)
+5. **Assembly quality assessment** (CheckM2, QUAST, BUSCO)
+6. **Genome annotation** (Prokka, Bakta)
+
+---
+
+## Step 1: Download Raw Reads
+
+We use [grabseqs](https://github.com/czbiohub/grabseqs) to download sequencing data from SRA.
+
+### 1.1. Install grabseqs
 
 ```bash
 # install grabseqs
@@ -19,19 +73,148 @@ conda install bioconda::sra-tools -y
 grabseqs --help
 ```
 
-#### 1.2. Download raw reads
+### 1.2. Download raw reads
 
 ```bash
 conda activate grabseqs
-# to download a sequence illumina run
+# Download Illumina short reads
 grabseqs sra -t 4 -m metadata.csv -o ./01_raw_reads/short_reads -r 4 SRR8893090
 
-# first run for nanopor reads
+# Download Nanopore long reads (two runs)
 grabseqs sra -t 4 -m metadata.csv -o ./01_raw_reads/long_reads -r 4 SRR8893087
-# second run for nanopore reads
 grabseqs sra -t 4 -m metadata.csv -o ./01_raw_reads/long_reads -r 4 SRR8893086
-# pacbio reads
+
+# Download PacBio long reads
 grabseqs sra -t 4 -m metadata.csv -o ./01_raw_reads/long_reads -r 4 SRR8893091
 ```
+
+---
+
+## Step 2: Tool Installation
+
+All required tools are installed using conda environments for reproducibility. The script [`installation.sh`](./installation.sh) automates this process.
+
+### 2.1. Run the installation script
+
+```bash
+bash installation.sh
+```
+
+This script sets up environments for:
+
+- Short read QC: `fastqc`, `fastp`
+- MultiQC for report aggregation
+- Long read QC: `NanoPlot`, `NanoFilt`, `Filtlong`
+- Assembly: `Unicycler`
+- Quality assessment: `CheckM2`, `QUAST`, `BUSCO`
+- Annotation: `Prokka`, `Bakta`
+
+It also downloads necessary databases for CheckM2 and Bakta.
+
+---
+
+## Step 3: Data Preparation and Quality Control
+
+All analysis steps are automated in [`analysis.sh`](./analysis.sh). Below, each step is explained.
+
+### 3.1. Rename and organize raw reads
+
+Raw reads are renamed for consistency and directories are created for each analysis stage.
+
+### 3.2. Short Read Quality Control
+
+- **FastQC**: Generates quality reports for raw short reads.
+- **MultiQC**: Aggregates FastQC reports for easy review.
+
+### 3.3. Short Read Preprocessing
+
+- **fastp**: Performs quality trimming and filtering of short reads, generating processed files and reports.
+
+### 3.4. Long Read Quality Control
+
+- **NanoPlot**: Visualizes quality metrics for raw and processed long reads.
+
+### 3.5. Long Read Preprocessing
+
+- **NanoFilt**: Filters long reads by quality and length, and crops low-quality bases.
+- **Filtlong**: (Optional, commented) Further filters long reads by length and quality.
+
+---
+
+## Step 4: Genome Assembly
+
+- **Unicycler** is used for all assembly strategies:
+  - **Short-read only**
+  - **Long-read only**
+  - **Hybrid (short + long reads)**
+
+Each assembly is output to a separate directory.
+
+---
+
+## Step 5: Genome Quality Assessment
+
+Three tools are used to assess assembly quality:
+
+- **CheckM2**: Estimates completeness and contamination.
+- **QUAST**: Provides assembly statistics and gene finding.
+- **BUSCO**: Assesses completeness using single-copy orthologs.
+
+Each tool is run on all three assemblies (short, long, hybrid).
+
+---
+
+## Step 6: Genome Annotation
+
+Two annotation tools are provided:
+
+- **Prokka**: Rapid prokaryotic genome annotation.
+- **Bakta**: Modern annotation tool with up-to-date databases.
+
+Both tools are run on all assemblies.
+
+---
+
+## Running the Analysis
+
+After installing tools, run the analysis script:
+
+```bash
+bash analysis.sh
+```
+
+This script will:
+
+1. Prepare directories and rename files
+2. Run all QC, preprocessing, assembly, quality assessment, and annotation steps as described above
+
+---
+
+## Script Reference
+
+- [`installation.sh`](./installation.sh): Installs all required tools and databases.
+- [`analysis.sh`](./analysis.sh): Runs the full analysis workflow, step by step.
+
+---
+
+## Notes
+
+- Adjust thread counts (`-t`, `-c`, `-w`) as appropriate for your hardware.
+- Ensure all database paths are correct and accessible.
+- For BUSCO, download the required lineage datasets as needed.
+
+---
+
+## Troubleshooting
+
+- If a conda environment fails to activate within a script, ensure you are using `eval "$(conda shell.bash hook)"` at the start of your shell session or script.
+- Database downloads may require sufficient disk space and a stable internet connection.
+
+---
+
+## Acknowledgements
+
+This guide was prepared for educational purposes to help students understand and perform bacterial genome assembly using modern bioinformatics tools.
+
 ---
 
